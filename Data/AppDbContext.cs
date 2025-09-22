36ed36283de171546dbdeee24341cc4348125937
Mike <mikekramer0316@gmail.com>
2025-09-22 09:33:00 +0200

    Initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmFeed.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<ProductieBeheerder> ProductieBeheerders  { get; set; }
+        public DbSet<Building> Buildings { get; set; } 
+
+         
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;port=3306;user=root;password=;database=csd_FarmFeed",
+                ServerVersion.Parse("8.0.30")
+            );
+        }
+
+    }
+}
